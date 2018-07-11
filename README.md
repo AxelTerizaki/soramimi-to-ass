3c783fb53033578ad8f774bf5bb140ac596b979a
Guillaume Lebigot <axel@teri-chan.net>
2018-07-11 12:24:26 +0200

    Looks like a good version 1

---
+++ README.md
@@ -0,0 +1,48 @@
+# Soramimi karaoke to ASS converter
+
+This nodeJS module converts Soramimi karaoke files (.txt) to Substation Alpha/Aegisub files (.ass).
+
+Soramimi karaoke's software and lyric files can be found at their official site
+
+* [http://www.soramimi.nl/](http://www.soramimi.nl/)
+
+It applies a basic style to it which can be modified later.
+
+## Install
+
+### Command-line
+
+`npm install -g soramimi-to-ass`
+
+### Module
+
+`npm install soramimi-to-ass`
+
+## Usage
+
+### Command-line
+
+Just supply the name of a .txt Soramimi file as your first argument, wether you call it in command line or as a module.
+
+### Module
+
+Supply the main function with the Soramimi karaoke contents.
+
+```
+import convertToASS from 'soramimi-to-ass';
+
+...
+
+const file = 'karaoke.txt';
+const soramimiData = await fs.readFile(file, 'utf8');
+const ass = convertToASS(soramimiData);
+console.log(ass);
+```
+
+## TODO
+
+* Write a better/simpler converter
+* Make default style variable
+* Also convert HTML font color tags and apply them via a style. (for now it's ignored)
+* Add checks for format
+* Other ideas? PR/MRs and issues are welcome.
--- index.js
+++ index.js
@@ -13,34 +13,41 @@ function asyncRead(...args){
 	return util.promisify(fs.readFile)(...args);
 }
 
+function clone(a) {
+	return JSON.parse(JSON.stringify(a));
+}
+
 function convertTimeToASSFormat(str) {
-	const pos = str.lastIndexOf(':');
-	str[pos] = '.';
-	return str;
+	return '0:' + str.replace(/:([^:]*)$/,'.$1');
 }
 
 function timeToMs(time) {
-	const a = time.split(':'); // split it at the colons	
+	const a = time.split(':'); // split it at the colons
 	// minutes are worth 60 seconds. Seconds are worth 100 miliseconds.
 	return (+a[0]) * 60 * 100 + (+a[1]) * 100 + (+a[2]);
 }
 
-function isOdd(num) { return (num % 2) == 1;}
+function isOdd(num) {
+	return (num % 2) == 1;
+}
 
-async function convertToASS() {
+async function mainCLI() {
 	const file = process.argv[2];
+	if (!await asyncExists(file)) throw `File ${file} does not exist`;
 	const soramimi = await asyncRead(file, 'utf8');
+	return convertToASS(soramimi);
+}
+
+function convertToASS(soramimi) {
 	// Removing HTML tags
 	const soramimiStripped = stripTags(soramimi);
 	const lines = soramimiStripped.split('\n');
-	let out = [ass.ScriptInfo, ass.Styles];
 	let dialogue = [];
-	for (const y in lines) {		
-		console.log(y);
+	for (const y in lines) {
 		if (lines[y] === '') continue;
 		let event = {};
-		event = Object.assign(ass.Dialogue);
-		const arr = lines[y].replace(/\[/g,'|').replace(/\]/g,'|').split('|');			
+		event = clone(ass.Dialogue);
+		const arr = lines[y].replace(/\[/g,'|').replace(/\]/g,'|').split('|');
 		event.value.Start = convertTimeToASSFormat(arr[1]);
 		event.value.End = convertTimeToASSFormat(arr[arr.length-2]);
 		//Building text
@@ -48,17 +55,19 @@ async function convertToASS() {
 		for (let i = 2; i<arr.length-1; i++) {
 			if (!isOdd(i)) {
 				const k = timeToMs(arr[i+1]) - timeToMs(arr[i-1]);
-				textArr.push(`{\k${k}}${arr[i]}`);
+				textArr.push(`{\\k${k}}${arr[i]}`);
 			}
 		}
-		event.value.Text = textArr.join('');		
-		dialogue.push(Object.assign(event));				
+		event.value.Text = textArr.join('');
+		dialogue.push(clone(event));
 	}
-	let events = Object.assign(ass.Events);
+	let events = clone(ass.Events);
 	events.body = events.body.concat(dialogue);
-	out.push(events);
-	return stringify(out);
+	return stringify([ass.ScriptInfo, ass.Styles, events]);
 }
 
+if (require.main === module) mainCLI()
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
 
-convertToASS().then(data => console.log(data));
+module.exports = convertToASS;
